326c25d0f028414c348b8fa2238371d134965a6f
Fernando Mumbach <fermuch@ayvu.net>
2022-01-28 17:37:02 -0300

    reading minew data

--- android/src/main/java/com/fernandomumbach/reactnativebeaconscanner/RNBeaconScannerModule.kt
+++ android/src/main/java/com/fernandomumbach/reactnativebeaconscanner/RNBeaconScannerModule.kt
@@ -4,70 +4,91 @@ import android.content.Context
 import android.util.Log
 import androidx.annotation.Nullable
 import com.facebook.react.bridge.*
-import org.altbeacon.beacon.*
 
 import com.facebook.react.modules.core.DeviceEventManagerModule.RCTDeviceEventEmitter
 import java.util.*
+import com.minew.beaconplus.sdk.MTCentralManager
+import com.minew.beaconplus.sdk.MTPeripheral
+import com.minew.beaconplus.sdk.interfaces.MTCentralManagerListener
+import com.minew.beaconplus.sdk.frames.MinewFrame
+
+import java.util.ArrayList
+
+import com.minew.beaconplus.sdk.MTFrameHandler
+import com.minew.beaconplus.sdk.enums.FrameType
+import com.minew.beaconplus.sdk.frames.AccFrame
+import com.minew.beaconplus.sdk.frames.DeviceInfoFrame
+import com.minew.beaconplus.sdk.frames.IBeaconFrame
 
 
 class RNBeaconScannerModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext),
-    RangeNotifier {
+    MTCentralManagerListener {
     private val LOG_TAG = "RNBeaconScannerModule"
 
-    private var mBeaconManager: BeaconManager? = null
+    private var mtCentralManager: MTCentralManager? = null
     private var mApplicationContext: Context? = null
     private var mReactContext: ReactApplicationContext? = null
-    private var mGlobalRegion: Region
-
-    private val IBEACON_LAYOUT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"
-    private val ALTBEACON_LAYOUT = BeaconParser.ALTBEACON_LAYOUT
 
     override fun getName() = LOG_TAG
 
     init {
         mReactContext = reactContext
-        mGlobalRegion = Region("all-beacons-region", null, null, null)
     }
 
     override fun initialize() {
         mApplicationContext = mReactContext!!.applicationContext
-        mBeaconManager = this.beaconManager(mApplicationContext!!)
+        mtCentralManager = MTCentralManager.getInstance(mApplicationContext!!)
 
-        mBeaconManager!!.addRangeNotifier(this)
+        mtCentralManager?.setMTCentralManagerListener(this)
     }
 
     /***** CALLBACKS ******/
 
-    override fun didRangeBeaconsInRegion(beacons: MutableCollection<Beacon>?, region: Region?) {
-        Log.w(LOG_TAG, "Beacons " + beacons.toString())
+    override fun onScanedPeripheral(peripherals: List<MTPeripheral>) {
         val arr = Arguments.createArray()
 
-        beacons?.iterator()?.forEach {
+        for (mtPeripheral in peripherals) {
             val bMap = Arguments.createMap()
-            bMap.putString("address", it.bluetoothAddress)
-            bMap.putString("name", it.bluetoothName)
-            bMap.putDouble("distance", it.distance)
-            bMap.putInt("rssi", it.rssi)
-            bMap.putDouble("avgRSSI", it.runningAverageRssi)
-            bMap.putInt("manufacturer", it.manufacturer)
-            bMap.putInt("scanCount", it.packetCount)
-            bMap.putString("uuid", it.id1.toUuid().toString())
-            bMap.putInt("major", it.id2.toInt())
-            bMap.putInt("minor", it.id3.toInt())
+            val mtFrameHandler = mtPeripheral.mMTFrameHandler
 
-            val dataArr = Arguments.createArray()
-            it.dataFields.iterator().forEach {
-                dataArr.pushDouble(it.toDouble())
+            Log.w(LOG_TAG, "found mac: " + mtFrameHandler.mac)
+
+            bMap.putString("mac", mtFrameHandler.mac)
+            bMap.putString("name", mtFrameHandler.name)
+            bMap.putInt("battery", mtFrameHandler.battery)
+            bMap.putInt("rssi", mtFrameHandler.rssi)
+            bMap.putDouble("lastUpdate", mtFrameHandler.lastUpdate.toDouble())
+
+            val dataFrames = Arguments.createArray()
+            val advFrames = mtFrameHandler.advFrames
+
+            advFrames.forEach {
+                val frameType = it.frameType
+                when (frameType) {
+                    FrameType.FrameiBeacon -> {
+                        it as IBeaconFrame
+                        val iBeaconMap = Arguments.createMap()
+                        iBeaconMap.putString("type", "ibeacon")
+                        iBeaconMap.putString("uuid", it.uuid)
+                        iBeaconMap.putInt("major", it.major)
+                        iBeaconMap.putInt("minor", it.minor)
+                        iBeaconMap.putInt("tx", it.txPower)
+                        dataFrames.pushMap(iBeaconMap)
+                    }
+
+                    FrameType.FrameAccSensor -> {
+                        it as AccFrame
+                        val accMap = Arguments.createMap()
+                        accMap.putString("type", "accelerometer")
+                        accMap.putDouble("x", it.xAxis)
+                        accMap.putDouble("y", it.yAxis)
+                        accMap.putDouble("z", it.zAxis)
+                        dataFrames.pushMap(accMap)
+                    }
+                }
             }
-            bMap.putArray("data", dataArr)
 
-            val extraArr = Arguments.createArray()
-            it.extraDataFields.iterator().forEach {
-                extraArr.pushDouble(it.toDouble())
-            }
-            bMap.putArray("extra", extraArr)
-
-            bMap.putString("16bitUUID", UUID.nameUUIDFromBytes(it.serviceUuid128Bit).toString())
+            bMap.putArray("frames", dataFrames)
 
             arr.pushMap(bMap)
         }
@@ -75,20 +96,46 @@ class RNBeaconScannerModule(reactContext: ReactApplicationContext) : ReactContex
         mReactContext?.let { sendEventArray(it, "beacons", arr) }
     }
 
+//    override fun didRangeBeaconsInRegion(beacons: MutableCollection<Beacon>?, region: Region?) {
+//        Log.w(LOG_TAG, "Beacons " + beacons.toString())
+//        val arr = Arguments.createArray()
+//
+//        beacons?.iterator()?.forEach {
+//            val bMap = Arguments.createMap()
+//            bMap.putString("address", it.bluetoothAddress)
+//            bMap.putString("name", it.bluetoothName)
+//            bMap.putDouble("distance", it.distance)
+//            bMap.putInt("rssi", it.rssi)
+//            bMap.putDouble("avgRSSI", it.runningAverageRssi)
+//            bMap.putInt("manufacturer", it.manufacturer)
+//            bMap.putInt("scanCount", it.packetCount)
+//            bMap.putString("uuid", it.id1.toUuid().toString())
+//            bMap.putInt("major", it.id2.toInt())
+//            bMap.putInt("minor", it.id3.toInt())
+//
+//            val dataArr = Arguments.createArray()
+//            it.dataFields.iterator().forEach {
+//                dataArr.pushDouble(it.toDouble())
+//            }
+//            bMap.putArray("data", dataArr)
+//
+//            val extraArr = Arguments.createArray()
+//            it.extraDataFields.iterator().forEach {
+//                extraArr.pushDouble(it.toDouble())
+//            }
+//            bMap.putArray("extra", extraArr)
+//
+//            bMap.putString("16bitUUID", UUID.nameUUIDFromBytes(it.serviceUuid128Bit).toString())
+//
+//            arr.pushMap(bMap)
+//        }
+//
+//        mReactContext?.let { sendEventArray(it, "beacons", arr) }
+//    }
+
     /***** END CALLBACKS ******/
 
     /***** UTILS ******/
-
-    private fun beaconManager(ctx: Context): BeaconManager {
-        val instance = BeaconManager.getInstanceForApplication(ctx)
-
-        // Add all the beacon types we want to discover
-//        instance.beaconParsers.add(BeaconParser().setBeaconLayout(IBEACON_LAYOUT))
-//        instance.beaconParsers.add(BeaconParser().setBeaconLayout(ALTBEACON_LAYOUT))
-        instance.beaconParsers.add(BeaconParser().setBeaconLayout("m:0-1=a108,x"))
-
-        return instance
-    }
 
     private fun sendEvent(
         reactContext: ReactContext,
@@ -120,12 +167,14 @@ class RNBeaconScannerModule(reactContext: ReactApplicationContext) : ReactContex
 
     @ReactMethod
     fun start() {
-        mBeaconManager?.startRangingBeacons(mGlobalRegion)
+        mtCentralManager?.startService()
+        mtCentralManager?.startScan()
     }
 
     @ReactMethod
     fun stop() {
-        mBeaconManager?.stopRangingBeacons(mGlobalRegion)
+        mtCentralManager?.stopService()
+        mtCentralManager?.stopScan()
     }
 
     /***** END REACT METHODS ******/
